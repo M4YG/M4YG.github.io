2ba1f5d76e01d642442ed6af21c6bdc32770ea1d
Mateus Yagami <jorgeinhoganer@gmail.com>
2022-12-05 12:19:11 -0300

    Indexs

---
+++ consulta1.js
@@ -0,0 +1,47 @@
+var dataRef = new Firebase('https://marcos-5491e-default-rtdb.firebaseio.com/escritorio_de_advocacia');
+
+dataRef.on('value', function(snapshot) {
+
+    var t = snapshot.val();
+    var count = 0;
+
+    for (var key in t) {
+        var valor = t[key];
+
+        console.log('chave', key);
+        console.log('valor', valor);
+
+        if(key === 'clientes') {
+
+            var _itemsKey = Object.entries(valor);
+    
+    
+    
+            for (const [key2, value2] of _itemsKey) {
+                count++;
+                
+                $("#tbclientes tbody").append(
+    
+                    `<tr> +
+    
+                        <th scope="row">${count}</th> +
+    
+                        <td>${value2.nome}</td> +
+    
+                        <td>${value2.telefone}</td> +
+    
+                        <td>${value2.email}</td> +
+
+                        <td>${value2.cpf}</td> +
+    
+                    </tr>`
+    
+                );
+    
+            }
+        }
+
+
+    }
+
+});
